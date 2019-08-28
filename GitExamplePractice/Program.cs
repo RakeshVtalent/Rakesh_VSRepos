0135ec5b20276504a50e47f6f07d71e9ae155b5c
RakeshVtalent <rakeshkumar.vtalent@gmail.coom>
2019-08-28 12:06:26 +0530

    main program written

---
+++ GitExamplePractice/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GitExamplePractice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x =1, y =2 , z;
+            z = x + y;
+        }
+    }
+}
